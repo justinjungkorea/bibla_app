0c9e3a7c3f11cb9f2c0e523b5443c189772eb0af
Justin_Jung <jungdw0624@gmail.com>
2019-05-02 20:30:26 +0900

    Updates

---
+++ precache-manifest.1f026ae7650739c548de8382f5242f75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18873d4637ce3a19ac6f6769687653f1",
+    "url": "/bibla_app/index.html"
+  },
+  {
+    "revision": "a1f5c7b0b11d1ca471e0",
+    "url": "/bibla_app/static/css/main.58f647af.chunk.css"
+  },
+  {
+    "revision": "789b047bce8191443bcf",
+    "url": "/bibla_app/static/js/2.eddfb069.chunk.js"
+  },
+  {
+    "revision": "a1f5c7b0b11d1ca471e0",
+    "url": "/bibla_app/static/js/main.9bffd079.chunk.js"
+  },
+  {
+    "revision": "9d6c1fda034aad26481f",
+    "url": "/bibla_app/static/js/runtime~main.ece8b4a1.js"
+  }
+]);
