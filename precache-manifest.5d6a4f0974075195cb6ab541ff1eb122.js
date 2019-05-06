dde43315a1bc75335ab71b9a9ea0e62b23e9cf03
Justin_Jung <jungdw0624@gmail.com>
2019-05-06 16:24:05 +0900

    Updates

---
+++ precache-manifest.5d6a4f0974075195cb6ab541ff1eb122.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba52e71c4c45797e59f5291e44b5544",
+    "url": "/bibla_app/index.html"
+  },
+  {
+    "revision": "c582eb88ddeb73fccb5f",
+    "url": "/bibla_app/static/css/main.3a6c636b.chunk.css"
+  },
+  {
+    "revision": "789b047bce8191443bcf",
+    "url": "/bibla_app/static/js/2.eddfb069.chunk.js"
+  },
+  {
+    "revision": "c582eb88ddeb73fccb5f",
+    "url": "/bibla_app/static/js/main.20a4e633.chunk.js"
+  },
+  {
+    "revision": "9d6c1fda034aad26481f",
+    "url": "/bibla_app/static/js/runtime~main.ece8b4a1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/bibla_app/precache-manifest.6a01401e9a17afed5093c3f433d4dea1.js"
+  "/bibla_app/precache-manifest.5d6a4f0974075195cb6ab541ff1eb122.js"
 );
 
 self.addEventListener('message', (event) => {
