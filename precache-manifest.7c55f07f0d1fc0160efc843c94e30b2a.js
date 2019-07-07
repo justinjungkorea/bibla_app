7a8d1e50e61117a778be51a8062e8eb36646368f
Dong Wook Jung <jungdw0624@gmail.com>
2019-07-07 21:05:13 +0900

    Updates

--- precache-manifest.7c55f07f0d1fc0160efc843c94e30b2a.js
+++ precache-manifest.7c55f07f0d1fc0160efc843c94e30b2a.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0a0765f38c83962e3b7b872eedeaa893",
+    "revision": "9a937603ed7ed4072822c9010a51c4f0",
     "url": "/bibla_app/index.html"
   },
   {
-    "revision": "4e2f1f5b6881c51275bf",
-    "url": "/bibla_app/static/css/main.0d46352e.chunk.css"
+    "revision": "873f398c3120b06252af",
+    "url": "/bibla_app/static/css/main.6190d466.chunk.css"
   },
   {
     "revision": "7fc95fd9da58eda1d6aa",
     "url": "/bibla_app/static/js/2.b086776d.chunk.js"
   },
   {
-    "revision": "4e2f1f5b6881c51275bf",
+    "revision": "873f398c3120b06252af",
     "url": "/bibla_app/static/js/main.6b68ed01.chunk.js"
   },
   {
