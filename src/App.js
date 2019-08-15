da06c3367b573313f8f220575bb57af49f598d72
Dong Wook Jung <jungdw0624@gmail.com>
2019-08-15 20:51:43 +0900

    code!

--- src/App.js
+++ src/App.js
@@ -258,7 +258,7 @@ class App extends Component {
     document.body.appendChild(el);
     window.getSelection().removeAllRanges();
     var range = document.createRange();
-    range.selectNode(el);
+    range.selectNodeContents(el);
     window.getSelection().addRange(range);
     document.execCommand("Copy");
     alert("클립보드에 복사 완료!");
